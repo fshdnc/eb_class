add loss file, in the middle of fixing double softmax bug

finnessayscore/evaluate.py
@@ -66,10 +66,8 @@ def _evaluate_seg_essay(dataloader, model, label_map):
         # accuracy
         preds = [v for item in preds for k,vs in item.items() for v in vs]
         preds = [int(torch.argmax(pred)) for pred in preds]
-        preds = [label_map["lab_grade"][p] for p in preds]
         #values, preds = torch.max(torch.tensor(preds), dim=1)
         target = [int(tt) for t in target for tt in t]
-        target = [label_map["lab_grade"][p] for p in target]
         overflow2sample_mapping = [ii for i in overflow2sample_mapping for ii in i]
 
     # mapping [1,1,2,3,3,3] preds [3,4,3,5,5,4] -> preds [4,3,5]
@@ -80,10 +78,13 @@ def _evaluate_seg_essay(dataloader, model, label_map):
             dict_pred[i] = [p]; dict_target[i] = [t]
         else:
             dict_pred[i].append(p); dict_target[i].append(t)
-    for i in sorted(set([overflow2sample_mapping])): # maybe sort is not required, but let's put it here
+    new_preds = []; new_target = []
+    for i in sorted(set(overflow2sample_mapping)): # maybe sort is not required, but let's put it here
         new_preds.append(round(numpy.mean(dict_pred[i])))
         new_target.append(round(numpy.mean(dict_target[i])))
-    return numpy.array(new_preds), numpy.array(new_target)
+    new_preds = [label_map["lab_grade"][p] for p in new_preds]
+    new_target = [label_map["lab_grade"][p] for p in new_target]
+    return new_preds, new_target
 
 def evaluate(dataloader, model, label_map, model_type, plot_conf_mat=False):
     if model_type == "seg_essay":

finnessayscore/model.py
@@ -4,7 +4,7 @@ import transformers
 import torch
 import torchmetrics
 import gc
-from loss import LabelSmoothingLoss
+from finnessayscore.loss import LabelSmoothingLoss
 
 class AbstractModel(pl.LightningModule):
 
@@ -85,7 +85,6 @@ class ClassModel(AbstractModel):
         super().__init__()
         self.bert = transformers.BertModel.from_pretrained(bert_model)
         self.cls_layers = torch.nn.ModuleDict({name: torch.nn.Linear(self.bert.config.hidden_size, len(lst)) for name, lst in class_nums.items()})
-        self.softmax = torch.nn.Softmax(dim=1)
         self.train_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.val_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.train_qwk = torch.nn.ModuleDict({name: torchmetrics.CohenKappa(num_classes=len(lst), weights='quadratic') for name, lst in class_nums.items()})
@@ -112,7 +111,7 @@ class ClassModel(AbstractModel):
                 first = False
             else:
                 enc = torch.cat((enc, sample_enc), dim=0)
-        return {name: self.softmax(layer(enc)) for name, layer in self.cls_layers.items()}
+        return {name: layer(enc) for name, layer in self.cls_layers.items()}
 
 
 class WholeEssayClassModel(AbstractModel):
@@ -125,7 +124,7 @@ class WholeEssayClassModel(AbstractModel):
         super().__init__()
         self.bert = transformers.BertModel.from_pretrained(bert_model)
         self.cls_layers = torch.nn.ModuleDict({name: torch.nn.Linear(self.bert.config.hidden_size, len(lst)) for name, lst in class_nums.items()})
-        self.softmax = torch.nn.Softmax(dim=1)
+        #self.softmax = torch.nn.Softmax(dim=1)
         self.train_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.val_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.train_qwk = torch.nn.ModuleDict({name: torchmetrics.CohenKappa(num_classes=len(lst), weights='quadratic') for name, lst in class_nums.items()})
@@ -156,7 +155,8 @@ class WholeEssayClassModel(AbstractModel):
         #for name, layer in self.cls_layers.items():
         #    print("cls layer(essay_enc)", layer(essay_enc))
         #    print("softmax cls essay_enc", self.softmax(layer(essay_enc)))
-        return {name: self.softmax(layer(essay_enc)) for name, layer in self.cls_layers.items()}
+        #return {name: self.softmax(layer(essay_enc)) for name, layer in self.cls_layers.items()}
+        return {name: layer(essay_enc) for name, layer in self.cls_layers.items()}
 
 
 class TruncEssayClassModel(AbstractModel):
@@ -169,7 +169,7 @@ class TruncEssayClassModel(AbstractModel):
         super().__init__()
         self.bert = transformers.BertModel.from_pretrained(bert_model)
         self.cls_layers = torch.nn.ModuleDict({name: torch.nn.Linear(self.bert.config.hidden_size, len(lst)) for name, lst in class_nums.items()})
-        self.softmax = torch.nn.Softmax(dim=1)
+        #self.softmax = torch.nn.Softmax(dim=1)
         self.train_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.val_acc = torch.nn.ModuleDict({name: torchmetrics.Accuracy() for name in class_nums})
         self.train_qwk = torch.nn.ModuleDict({name: torchmetrics.CohenKappa(num_classes=len(lst), weights='quadratic') for name, lst in class_nums.items()})
@@ -188,7 +188,8 @@ class TruncEssayClassModel(AbstractModel):
         enc = self.bert(input_ids=batch['input_ids'],
                         attention_mask=batch['attention_mask'],
                         token_type_ids=batch['token_type_ids']) #BxS_LENxSIZE; BxSIZE
-        return {name: self.softmax(layer(enc.pooler_output)) for name, layer in self.cls_layers.items()}
+        #return {name: self.softmax(layer(enc.pooler_output)) for name, layer in self.cls_layers.items()}
+        return {name: layer(enc.pooler_output) for name, layer in self.cls_layers.items()}
 
 
 class ProjectionClassModel(AbstractModel):