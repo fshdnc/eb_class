4af1ed7f90add3b195360eb171d5768bf4a43db4
Li-Hsin Chang <li-hsin.l.chang@utu.fi>
2021-09-20 18:35:30 +0300

    seg essay explainability code debugged by Filip

--- finnessayscore/explain.py
+++ finnessayscore/explain.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import torch.nn
 
 from captum.attr import visualization as viz
 from captum.attr import IntegratedGradients, LayerConductance, LayerIntegratedGradients
@@ -12,20 +13,44 @@ from finnessayscore import data_reader, model
 import pickle
 import argparse
 
+class Reshape(torch.nn.Module):
+
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(self.shape)
+
 # LayerIntegratedGradients only takes in inputs (tensor or tuple of tensors)
 class ExplainWholeEssayClassModel(model.WholeEssayClassModel):
     def __init__(self, class_nums, class_weights=None, **config):
         super().__init__(class_nums, class_weights=class_weights, **config)
+
+    #this really must have throughout as many inputs/outputs as what Captum sees
     def forward(self, batch_input_ids, batch_attention_mask, batch_token_type_ids):
+        essay_enc = self.bert(input_ids=batch_input_ids,
+                              attention_mask=batch_attention_mask,
+                              token_type_ids=batch_token_type_ids) #BxS_LENxSIZE; BxSIZE
+        #so the pooler out is now (fragments x 768)
+        essay_enc = torch.sum(essay_enc.pooler_output, 0).expand((*essay_enc.pooler_output.shape))
+        #so now we should have (fragments x 768) still, copied
+        print("ESSAY ENC SHAPE",essay_enc.shape,essay_enc[:,:3])
+        res= {name: layer(essay_enc) for name, layer in self.cls_layers.items()}
+        return res
+        
+    def forward_old(self, batch_input_ids, batch_attention_mask, batch_token_type_ids):
         print("batch_input_ids", batch_input_ids)
         print("batch_attention_mask", batch_attention_mask)
         print("batch_token_type_ids", batch_token_type_ids)
-        essay_enc = self.bert(input_ids=batch_input_ids[0],
-                        attention_mask=batch_attention_mask[0],
-                              token_type_ids=batch_token_type_ids[0]) #BxS_LENxSIZE; BxSIZE
+        essay_enc = self.bert(input_ids=batch_input_ids.squeeze(0),
+                              attention_mask=batch_attention_mask.squeeze(0),
+                              token_type_ids=batch_token_type_ids.squeeze(0)) #BxS_LENxSIZE; BxSIZE
         essay_enc = torch.unsqueeze(torch.sum(essay_enc.pooler_output, 0), 0)
-        print("essay_enc", essay_enc)
-        return {name: layer(essay_enc) for name, layer in self.cls_layers.items()}
+        res= {name: layer(essay_enc) for name, layer in self.cls_layers.items()}
+        return res
+
+    
 
 def predict(input_ids, attention_mask, token_type_ids): #inputs, token_type_ids=None, position_ids=None, attention_mask=None):
     pred = trained_model(input_ids, attention_mask, token_type_ids)
@@ -42,9 +67,11 @@ def aggregate(inp,attrs,tokenizer):
     detokenized=[]
     for l in inp[0][0].cpu().tolist():
         detokenized.append(tokenizer.convert_ids_to_tokens(l))
+    print("ATTRS SHAPE",attrs.shape)
     attrs=attrs.cpu().tolist()
     
-    print("DETOKENIZED",detokenized)
+    #print("DETOKENIZED",detokenized)
+
     aggregated=[]
     for token,a_val in zip(detokenized[0],attrs): #One text from the batch at a time!
         if token.startswith("##"):
@@ -101,14 +128,14 @@ def predict_and_explain(trained_model, tokenizer, obj_batch):
     lig = LayerIntegratedGradients(predict, trained_model.bert.embeddings)
     print("obj_batch", obj_batch)
     assert len(obj_batch["input_ids"])==1 # the whole_essay model only takes 1 example at a time
-    prediction = predict(torch.nn.utils.rnn.pad_sequence(obj_batch["input_ids"],batch_first=True).to(device),
-                          torch.nn.utils.rnn.pad_sequence(obj_batch["attention_mask"],batch_first=True).to(device),
-                          torch.nn.utils.rnn.pad_sequence(obj_batch["token_type_ids"],batch_first=True).to(device),)
-    print("PREDICTIONS", prediction.shape)
+#    prediction = predict(torch.nn.utils.rnn.pad_sequence(obj_batch["input_ids"],batch_first=True).to(device),
+#                          torch.nn.utils.rnn.pad_sequence(obj_batch["attention_mask"],batch_first=True).to(device),
+#                          torch.nn.utils.rnn.pad_sequence(obj_batch["token_type_ids"],batch_first=True).to(device),)
+#    print("PREDICTIONS", prediction.shape)
     #for i, prediction in enumerate(predictions): # the whole_essay model only takes 1 example at a time, so index is 0
-    prediction_cls=int(torch.argmax(prediction))
-    print("Gold standard:", obj_batch["lab_grade"][0].shape)
-    print("Prediction:", ("1","2","3","4","5")[prediction_cls],"Weights:",prediction.tolist())
+#    prediction_cls=int(torch.argmax(prediction))
+#    print("Gold standard:", obj_batch["lab_grade"][0].shape)
+#    print("Prediction:", ("1","2","3","4","5")[prediction_cls],"Weights:",prediction.tolist())
     ref_input = build_ref(0, obj_batch, tokenizer, device)
     #inp = (obj_batch["input_ids"][i].unsqueeze(0)[0].to(device),
     #       obj_batch["attention_mask"][i].unsqueeze(0)[0].to(device),
@@ -116,13 +143,18 @@ def predict_and_explain(trained_model, tokenizer, obj_batch):
     inp = (torch.nn.utils.rnn.pad_sequence(obj_batch["input_ids"],batch_first=True).to(device),
            torch.nn.utils.rnn.pad_sequence(obj_batch["attention_mask"],batch_first=True).to(device),
            torch.nn.utils.rnn.pad_sequence(obj_batch["token_type_ids"],batch_first=True).to(device),)
-    all_tokens = [tokenizer.convert_ids_to_tokens(seg) for seg in inp[0][0]]
+#    all_tokens = [tokenizer.convert_ids_to_tokens(seg) for seg in inp[0][0]]
     #print("all_tokens", all_tokens)
     print("inp", inp[0].shape)
-    print("ref_input",ref_input[0].shape)
+    print("attm",inp[1].shape)
+    print("tt",inp[2].shape)
+    print("ref_input",ref_input[0].shape,ref_input[1].shape,ref_input[2].shape)
     print("I AM HERE")
+    inp=tuple([x.squeeze(0) for x in inp]) #I am actually squeezing the fake first dim out
+    ref_input=tuple([x.squeeze(0) for x in ref_input])
     for target, classname in ((4,"5"),):
-        attrs, delta = lig.attribute(inputs=inp,
+        print("INP SHAPE",inp[0].shape,inp[1].shape,inp[2].shape)
+        attrs = lig.attribute(inputs=inp,
                                      baselines=ref_input,
                                      return_convergence_delta=False,
                                      target=target,
@@ -133,9 +165,13 @@ def predict_and_explain(trained_model, tokenizer, obj_batch):
         #    print("saved")
         #except Exception as e:
         #    print(e)
+
+        print("ATTRS SHAPE",attrs.shape)
+        print("ATTRS",attrs[:,:3,:1])
+        sys.exit()
         attrs_sum = summarize_attributions(attrs)
         aggregated = aggregate(inp, attrs_sum, tokenizer)
-        print("aggregated", aggregated)
+        
         x=captum.attr.visualization.format_word_importances(all_tokens, attrs_sum)
         print("ATTRIBUTION WITH RESPECT TO",classname)
         print_aggregated(target, aggregated)
